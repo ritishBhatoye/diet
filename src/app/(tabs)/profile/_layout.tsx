b2b2d4d3fc16c1703ed97723507a2f55231d71c3
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-07-05 21:04:49 +0530

    Remove Profile component from the project to streamline codebase and enhance maintainability.

---
+++ src/app/(tabs)/profile/_layout.tsx
@@ -0,0 +1,29 @@
+import { images } from "@/constants";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import React from "react";
+import { Image } from "react-native";
+const ProfileLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          headerLeft: () => (
+            <Ionicons name="backspace-outline" color={"dark-100"} size={16} />
+          ),
+          headerRight: () => (
+            <Image
+              source={images.search}
+              className="size-7"
+              tintColor={"dark-100"}
+            />
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default ProfileLayout;
