fb05ee1fed62fb353041260d8afcba785b55c2f8
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-07-05 18:51:40 +0530

    Add react-native-appwrite dependency and update TabLayout for icon rendering
    - Included `react-native-appwrite` version `0.10.0` in package.json and package-lock.json.
    - Enhanced TabLayout component to utilize Ionicons for tab bar icons, improving navigation UI.
    - Removed obsolete SpaceMono-Regular font file from assets.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,31 @@
+import { Image } from "expo-image";
 import { Tabs } from "expo-router";
 import React from "react";
+import { View } from "react-native";
+
+const TabBarIcon = ({ focused, icon, title }: any) => (
+  <View>
+    <Image
+      source={icon}
+      className="size-7"
+      contentFit="contain"
+      tintColor={focused ? "#FE8C00" : "#5D5F6D"}
+    />
+  </View>
+);
 
 const TabLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} title="Home" icon={images.home} />
+          ),
+        }}
+      />
       <Tabs.Screen name="search" options={{ title: "Search" }} />
       <Tabs.Screen name="cart" options={{ title: "Cart" }} />
       <Tabs.Screen name="profile" options={{ title: "Profile" }} />
