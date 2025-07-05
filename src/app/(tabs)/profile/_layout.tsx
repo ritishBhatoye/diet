c20588d856d1b409d9d694bd6f4576dff1cb45dd
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-07-05 21:18:15 +0530

    Update ProfileLayout header title style for improved readability

--- src/app/(tabs)/profile/_layout.tsx
+++ src/app/(tabs)/profile/_layout.tsx
@@ -9,6 +9,7 @@ const ProfileLayout = () => {
       <Stack.Screen
         name="profile"
         options={{
+          headerTitleStyle: { fontSize: 18, fontWeight: 600 },
           headerTitle: "Profile",
           headerLeft: () => (
             <Ionicons name="backspace-outline" color={"dark-100"} size={16} />
