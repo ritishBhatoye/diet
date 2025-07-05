6a3ee6f7f9a9999969cf7098e0fdcb74c2c5f44f
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-07-05 18:35:31 +0530

    Update TabLayout to include titles for navigation screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const TabLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="search" />
-      <Tabs.Screen name="cart" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="cart" options={{ title: "Cart" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
 };
